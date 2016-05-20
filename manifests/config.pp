6a1820ee6c7e1c436f50ad43ddba9fcf41e643c1
Frédéric DROUET <fdrouet@exoplatform.com>
2016-05-20 23:03:21 +0200

    Fix dependency

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,28 +3,28 @@ class kibana::config {
     path    => "${kibana::params::config_file}",
     match   => "^port:",
     line    => "port: ${kibana::port}",
-    require => Exec["install-kibana-${kibana::version}"],
+    require => File["${kibana::install_dir}/kibana"],
     notify  => Service["kibana"],
   }
   file_line { 'kibana_host' :
     path    => "${kibana::params::config_file}",
     match   => "^host:",
     line    => "host: \"${kibana::host}\"",
-    require => Exec["install-kibana-${kibana::version}"],
+    require => File["${kibana::install_dir}/kibana"],
     notify  => Service["kibana"],
   }
   file_line { 'kibana_es_url' :
     path    => "${kibana::params::config_file}",
     match   => "^elasticsearch_url:",
     line    => "elasticsearch_url: \"${kibana::es_url}\"",
-    require => Exec["install-kibana-${kibana::version}"],
+    require => File["${kibana::install_dir}/kibana"],
     notify  => Service["kibana"],
   }
   file_line { 'kibana_log_file' :
     path    => "${kibana::params::config_file}",
     match   => "log_file:",
     line    => "log_file: /var/log/kibana/kibana.log",
-    require => Exec["install-kibana-${kibana::version}"],
+    require => File["${kibana::install_dir}/kibana"],
     notify  => Service["kibana"],
   }
 }
